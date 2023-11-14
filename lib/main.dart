38adf64a7d06620f19f4b8000a8c40ec2e5f394a
Ana-Luiza-Oliveira <ana.luiza.mota.oliveira@gmail.com>
2023-11-13 23:53:08 -0300

    Autenticação(corrigir)

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:promocoes/pages/auth_or_app_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Promoções',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        // Altere para a cor desejada
+        useMaterial3: true,
+      ),
+      home: AuthOrAppPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
